6513de87dfa0b4f69608bf697b458102bdd304ce
leeyj4805 <leeyj4805@gmail.com>
2021-04-13 14:11:40 +0900

    21.04.13 공휴일 데이터 추가 업로드

--- src/api/KoreaAllData.js
+++ src/api/KoreaAllData.js
@@ -1,18 +1,27 @@
 import React, { useState,useEffect } from 'react';
 import Axios from "axios";
-function KoreaAllData() {
+
+function KoreaAllData(res) {
   const [user, setUser] = useState([])
 
     useEffect(() => {
     Axios
       .get("https://api.manana.kr/calendar.json",)
       .then((res) => {
-        setUser(res.data[1].name);
+        setUser(res.data);
         console.log(res.data.name)
       })
   },[user])
+
+  const FontStyle = {
+  fontSize: "0.1rem",
+}
   return (
-      <p>{user}</p>
+      <>
+        {user.map((user) => (
+			<p style={FontStyle}>{user.name}</p>
+		))}
+      </>
   )
 }
 export default KoreaAllData;
