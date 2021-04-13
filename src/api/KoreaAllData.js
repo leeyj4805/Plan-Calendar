24108e9c3fbe63b87d30a56533259c9d7ec9180b
leeyj4805 <leeyj4805@gmail.com>
2021-04-13 13:12:43 +0900

    최종 완성

--- src/api/KoreaAllData.js
+++ src/api/KoreaAllData.js
@@ -1,23 +1,18 @@
-import React  from 'react';
+import React, { useState,useEffect } from 'react';
 import Axios from "axios";
-
-
-
 function KoreaAllData() {
-    const date = () => {
-         Axios
-        .get('http://api.manana.kr/calendar.json')
-        .then((res) => {
-            console.log(res);
-        }
-        );
-    };
+  const [user, setUser] = useState([])
 
-
-        return (
-                    <p>111</p>
-                ); 
-        }
-
-
+    useEffect(() => {
+    Axios
+      .get("https://api.manana.kr/calendar.json",)
+      .then((res) => {
+        setUser(res.data[1].name);
+        console.log(res.data.name)
+      })
+  },[user])
+  return (
+      <p>{user}</p>
+  )
+}
 export default KoreaAllData;
